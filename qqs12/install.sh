e84d952eb5eb3fb38cdb237a3f0ed5d3d9cd4cf2
StarSky919 <ss919p@gmail.com>
2026-01-29 18:21:20 +0800

    QQS

--- qqs12/install.sh
+++ qqs12/install.sh
@@ -1,9 +1,34 @@
-if test -e $PREFIX/bin/ffmpeg
-then
+#!/bin/sh
+
+set -euo pipefail
+
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+install_qqs() {
+  echo "正在下载 QQS, QQSC, NVPQQS..."
   curl -fsSL https://starsky919.xyz/qqs12/QQS -o $PREFIX/bin/QQS
   curl -fsSL https://starsky919.xyz/qqs12/QQSC -o $PREFIX/bin/QQSC
+  curl -fsSL https://starsky919.xyz/qqs12/NVPQQS -o $PREFIX/bin/NVPQQS
   chmod 0755 $PREFIX/bin/QQS
   chmod 0755 $PREFIX/bin/QQSC
-else
+  chmod 0755 $PREFIX/bin/NVPQQS
+}
+
+if exists ffmpeg; then
+  echo "FFmpeg 已安装"
+elif exists pkg; then
+  echo "未检测到 FFmpeg, 正在安装...'"
   pkg install ffmpeg
+else
+  echo "未检测到 FFmpeg ,正在下载..."
+  curl -fsSL https://starsky919.xyz/ffmpeg -o $PREFIX/bin/ffmpeg
+  chmod 0755 $PREFIX/bin/ffmpeg
 fi
+
+install_qqs
+
+echo "安装完成"
+
+QQS
